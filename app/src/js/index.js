d1eab4a6f8f12bb2944b6663cd8ea4526bd85d5c
Jairo <jairopereira_dias@hotmail.com>
2020-05-27 08:02:12 -0300

    jogo concluído

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -25,15 +25,15 @@ new Vue({
     atk(){
       var atkmonster = parseInt((Math.random() * 10) + (Math.random() * 2));
       var atkjogador = parseInt(Math.random() * 10);
-      this.arrayJogador.push(atkmonster);
-      this.arrayMonster.push(atkjogador);
+      this.arrayJogador.push("O Monstro inferiu: " + atkmonster + "% de dano no life do jogador");
+      this.arrayMonster.push("O jogador inferiu: " + atkjogador + "% de dano no life do monstro");
       this.returnLife(atkjogador, atkmonster);
     },
     atkEspecial(){
       var atkmonster = parseInt((Math.random() * 10));
       var atkjogador = parseInt(Math.random() * 10 + (Math.random() * 3));
-      this.arrayJogador.push(atkmonster);
-      this.arrayMonster.push(atkjogador);
+      this.arrayJogador.push("O Monstro inferiu: " + atkmonster + "% de dano no life do jogador");
+      this.arrayMonster.push("O jogador inferiu: " + atkjogador + "% de dano no life do monstro");
       this.returnLife(atkjogador, atkmonster);
     },
 
@@ -68,6 +68,26 @@ new Vue({
         this.cor = 'green';
         this.ganhador = 'Jogador Venceu !!';
       }
+    },
+    reiniciar(){
+      this.lifemonster = 100;
+      this.lifeplay = 100;
+      this.cor = '';
+      this.ganhador = '';
+      this.winner = false; 
+      while(this.arrayJogador.length) {
+        this.arrayJogador.pop();
+        this.arrayMonster.pop();
+      }
+      
+    },
+    curar(){
+      var atkmonster = parseInt((Math.random() * 10));
+      var curaJogador = parseInt(Math.random() * 10);
+      ((this.lifeplay + curaJogador) > 100) ? this.lifeplay = 100 : this.lifeplay += curaJogador;
+      this.arrayJogador.push("O jogador curou: " + curaJogador + "% de life");
+      this.arrayJogador.push("O monstro inferiu: " + atkmonster + "% de dano no life do jogador");
+      this.returnLife(0, atkmonster);
     }
   }
 });
