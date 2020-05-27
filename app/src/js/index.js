f82ac3cde6a2ed08a04d2299cb4218df064dce1d
Jairo <jairopereira_dias@hotmail.com>
2020-05-27 07:46:00 -0300

    Vue-js brincando com danos entre jogador vs monster

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -3,6 +3,11 @@ new Vue({
   data:{
     lifeplay: 100,
     lifemonster: 100,
+    arrayJogador: [],
+    arrayMonster: [],
+    winner: false,
+    cor: '',
+    ganhador: ''
   },
   computed:{
     life(){
@@ -13,6 +18,55 @@ new Vue({
     lifemonstro(){
       return{
         width: this.lifemonster + '%'
+      }
+    }
+  },
+  methods:{
+    atk(){
+      var atkmonster = parseInt((Math.random() * 10) + (Math.random() * 2));
+      var atkjogador = parseInt(Math.random() * 10);
+      this.arrayJogador.push(atkmonster);
+      this.arrayMonster.push(atkjogador);
+      this.returnLife(atkjogador, atkmonster);
+    },
+    atkEspecial(){
+      var atkmonster = parseInt((Math.random() * 10));
+      var atkjogador = parseInt(Math.random() * 10 + (Math.random() * 3));
+      this.arrayJogador.push(atkmonster);
+      this.arrayMonster.push(atkjogador);
+      this.returnLife(atkjogador, atkmonster);
+    },
+
+    returnLife(jogador, monster){ 
+      if(!this.winner){
+        if((this.lifeplay - monster) > 0){
+          this.lifeplay -= monster;
+        }else{
+          this.winnerPlay('monster')
+          return false;
+        }
+
+        if((this.lifemonster - jogador) > 0){
+          this.lifemonster -= jogador;
+        }else{
+          this.winnerPlay('play')
+          return false;
+        }
+      }
+      
+
+    },
+    winnerPlay(vencedor){
+      console.log(vencedor);
+      this.winner = true;
+      if(vencedor == 'monster'){
+        this.lifeplay = 0;
+        this.cor = 'red';
+        this.ganhador = 'Monstro Venceu !';
+      }else{
+        this.lifemonster = 0;
+        this.cor = 'green';
+        this.ganhador = 'Jogador Venceu !!';
       }
     }
   }
